a83b3b12a26b7a05e12059ada80b6c49201e2d1a
Tam Vu <tam_vu@onskyhealth.com>
2025-10-01 21:11:53 +0700

    *Update code

---
+++ ECUVotol/ECUVotolApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECUVotolApp.swift
+//  ECUVotol
+//
+//  Created by Tam Vu on 1/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ECUVotolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ECUVotolTests/ECUVotolTests.swift
@@ -0,0 +1,17 @@
+//
+//  ECUVotolTests.swift
+//  ECUVotolTests
+//
+//  Created by Tam Vu on 1/10/25.
+//
+
+import Testing
+@testable import ECUVotol
+
+struct ECUVotolTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
